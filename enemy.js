2afe43d186e1cf7325692d6c2eed5923d68b4968
k0miker <165369924+k0miker@users.noreply.github.com>
2024-09-10 17:20:17 +0200

    enemy weapon type, alive status

--- enemy.js
+++ enemy.js
@@ -1,24 +1,60 @@
 export default class Enemy {
-    constructor(x, y, player, imageSrc) {
+    constructor(x, y, w, h, speed, enemy, hp, imageSrc, weaponType) {
         this.x = x;
         this.y = y;
-        this.width = 50;
-        this.height = 50;
-        this.speed = 2 + Math.random() * 3;
+        this.width = w;
+        this.height = h;
+        this.speed = speed + Math.random() * 3;
         this.image = new Image();
         this.image.src = imageSrc;
-        this.player = player;
+        this.enemy = enemy;
+        this.health = hp; 
+        this.isAlive = true; 
+        this.weaponType = weaponType; 
     }
 
     update() {
-        let dx = this.player.x - this.x;
-        let dy = this.player.y - this.y;
-        let dist = Math.sqrt(dx * dx + dy * dy);
-        this.x += (dx / dist) * this.speed;
-        this.y += (dy / dist) * this.speed;
+        if (this.isAlive) {
+            let dx = this.enemy.x - this.x;
+            let dy = this.enemy.y - this.y;
+            let dist = Math.sqrt(dx * dx + dy * dy);
+            this.x += (dx / dist) * this.speed;
+            this.y += (dy / dist) * this.speed;
+        }
     }
 
     draw(ctx) {
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        if (this.isAlive) {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        }
+    }
+
+    takeDamage(damage) {
+        if (this.isAlive) {
+            this.health -= damage;
+            if (this.health <= 0) {
+                this.isAlive = false;
+            }
+        }
+    }
+
+    attack(target) {
+        if (this.isAlive) {
+            let damage;
+            switch (this.weaponType) {
+                case 'sword':
+                    damage = 10;
+                    break;
+                case 'bow':
+                    damage = 7;
+                    break;
+                case 'magic':
+                    damage = 12;
+                    break;
+                default:
+                    damage = 5;
+            }
+            target.takeDamage(damage);
+        }
     }
 }

5bf13533c56c9d73a593880b1e1a01e1b47888f6
Jörg <jbantin@gmx.de>
2024-09-10 16:11:12 +0200

    img add

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-console.log(ctx);
 
 console.log("dvlkjndbndbl");
+const img = new Image();
+img.src = "./assets/ground.png";
+img.addEventListener("load", () => {
+  ctx.drawImage(img, 0, 0);
+});

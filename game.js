989464407a33e6873a615246be4a44c8ab30d921
k0miker <165369924+k0miker@users.noreply.github.com>
2024-09-10 15:58:16 +0200

    .

--- game.js
+++ game.js
@@ -1,17 +1,7 @@
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 console.log(ctx);
-
-
-
-
-
-
-
-
-
-
-
-
-
+const ground = new Image();
+ground.src = 'img/ground.png';
+const 
 

2f610f4a6d69ba82f0daf2495d2959efb81de8bc
k0miker <165369924+k0miker@users.noreply.github.com>
2024-09-22 11:34:27 +0200

    .

--- Player.js
+++ Player.js
@@ -5,7 +5,7 @@ export default class Player {
     this.y = 350;
     this.width = 50;
     this.height = 50;
-    this.speed = 5;
+    this.speed = 8;
     this.image = new Image();
     this.health = 100;
     this.weaponType = "default";

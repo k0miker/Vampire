2b912b44adf5c8c5174185829c59b758d3e8ecc5
k0miker <165369924+k0miker@users.noreply.github.com>
2024-09-23 15:44:43 +0200

    working

--- Game2.js
+++ Game2.js
@@ -28,7 +28,7 @@ class Game {
     this.mouseY = this.canvas.height / 2;
     this.bullets = [];
     this.mapArray = mapArray;
-    this.mapIndex = { x: 1, y: 3 };
+    this.mapIndex = { x: 3, y:0  };
     this.currentMap = new mapArray[this.mapIndex.y][this.mapIndex.x]();
 
     this.mapHandler = new MapHandler(
